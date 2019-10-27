61496ef2064268b58a1e3da6e48df5321ba9887c
edgardo-web <edgardo.a.garcia.f@gmail.com>
2019-10-26 19:20:57 -0500

    Firebase config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-  production: true
+  production: true,
+  FireBaseConfig: {
+    apiKey: "AIzaSyDzht8R9wS4RLqfZCrPkTkhSYLChSuSJXo",
+    authDomain: "loginapp-aa941.firebaseapp.com",
+    databaseURL: "https://loginapp-aa941.firebaseio.com",
+    projectId: "loginapp-aa941",
+    storageBucket: "loginapp-aa941.appspot.com",
+    messagingSenderId: "791520396060",
+    appId: "1:791520396060:web:46a86d41f863b902dd30d9",
+    measurementId: "G-52YW395RP6"
+  }
+
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  FireBaseConfig: {
+  apiKey: "AIzaSyDzht8R9wS4RLqfZCrPkTkhSYLChSuSJXo",
+    authDomain: "loginapp-aa941.firebaseapp.com",
+    databaseURL: "https://loginapp-aa941.firebaseio.com",
+    projectId: "loginapp-aa941",
+    storageBucket: "loginapp-aa941.appspot.com",
+    messagingSenderId: "791520396060",
+    appId: "1:791520396060:web:46a86d41f863b902dd30d9",
+    measurementId: "G-52YW395RP6"}
 };
 
 /*
